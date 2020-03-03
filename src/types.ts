a1b9c0ec71da31e8f225035ffb7b756d6578a849
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-03-03 23:44:51 +0800

    add highlight range function

--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,21 @@
-export const getHtmlSelection: () => string | undefined = () => {
+export const getHtmlSelection = () => {
   const selection = window.getSelection()
   if (selection && selection.anchorNode) {
     const range = selection.getRangeAt(0);
-    const div = document.createElement("div");
-    div.appendChild(range.cloneContents());
-    return div.innerHTML;
-  } else {
-    return undefined
+    highlightRange(range)
+  }
+}
+
+export const highlightRange = (range: Range) => {
+  const treeWalker = document.createTreeWalker(range.commonAncestorContainer)
+  while (treeWalker.nextNode()) {
+    const currentNode = treeWalker.currentNode
+    if (currentNode.nodeType == Node.TEXT_NODE && range.intersectsNode(currentNode)) {
+      const div = document.createElement('div')
+      div.style.background = 'yellow'
+      div.style.width = 'fit-content'
+      div.appendChild(currentNode.cloneNode())
+      currentNode.parentNode.?.replaceChild(div, currentNode)
+    }
   }
 }
