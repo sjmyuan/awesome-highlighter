feb072f071743c6b8e27f1f5a9e0becd85a83568
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-03-04 14:00:05 +0800

    fix the issue of non-word character

--- src/types.ts
+++ src/types.ts
@@ -9,26 +9,35 @@ export const highlightSelection = () => {
 
 export const highlightRange = (range: Range) => {
   const root = range.commonAncestorContainer
-  const treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT)
   const textNodes = []
-  while (treeWalker.nextNode()) {
-    const currentNode: Node = treeWalker.currentNode
-    if (currentNode.nodeType == Node.TEXT_NODE && range.intersectsNode(currentNode)) {
-      textNodes.push(currentNode)
+  if (root.hasChildNodes()) {
+    const treeWalker = document.createTreeWalker(root, NodeFilter.SHOW_TEXT)
+    while (treeWalker.nextNode()) {
+      const currentNode: Node = treeWalker.currentNode
+      if (range.intersectsNode(currentNode)) {
+        textNodes.push(currentNode)
+      }
     }
+  } else if (root.nodeType === Node.TEXT_NODE) {
+    textNodes.push(root)
+  } else {
+    console.log('Can not process this range, the root dom is')
+    console.log(root)
   }
 
   console.log(textNodes)
   for (let index = 0; index < textNodes.length; index++) {
     const currentNode = textNodes[index];
     const currentParent = currentNode.parentNode
-    console.log(currentNode)
-    console.log(currentParent)
-    console.log('----')
-    const div = document.createElement('a')
-    div.style.background = 'yellow'
-    div.style.width = 'fit-content'
-    div.appendChild(currentNode.cloneNode())
-    currentParent?.replaceChild(div, currentNode)
+    if (currentNode.textContent && currentNode.textContent.trim().length > 0) {
+      console.log(currentNode)
+      console.log(currentParent)
+      console.log('----')
+      const div = document.createElement('a')
+      div.style.background = 'yellow'
+      div.style.width = 'fit-content'
+      div.appendChild(currentNode.cloneNode())
+      currentParent?.replaceChild(div, currentNode)
+    }
   }
 }
