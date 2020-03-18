4d7d13fc51a01832836e4f203019a9f029248a81
Jiaming Shang <shangjiaming.yuan@gmail.com>
2020-03-18 13:15:20 +0800

    use highlight style in popup

--- src/component/Highlight.tsx
+++ src/component/Highlight.tsx
@@ -4,14 +4,9 @@ import {HighlightInfo} from '../types';
 
 
 const Div = styled.div`
-  with: 100%;
+  with: auto;
   min-height: 1rem;
-  border: 1px solid;
-  border-radius: 6px;
-  margin: 10px 0px;
   padding: 1rem 1.5rem;
-  background-color: #e8d615;
-  box-shadow: 0px 0px 13px 0px rgba(199,188,64,1);
 `
 
 interface HighlightProps {
